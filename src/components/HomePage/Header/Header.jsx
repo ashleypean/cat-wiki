e97126835d962c7ef612ab9edab5a6df1484801b
Ashley Pean <pean.ashley@gmail.com>
2020-11-06 02:19:39 -0500

    Adjusted text sizing in footer for responsiveness with small mobile devices. Adjusted positioning of header image from conatin to cover

--- src/components/HomePage/Header/Header.jsx
+++ src/components/HomePage/Header/Header.jsx
@@ -9,7 +9,11 @@ export default function Header() {
       <h4 className="title">CatWiki</h4>
       <p className="subtitle">Get to know more about your cat breed</p>
       <div className="search">
-        <input type="text" name="" id="" autocomplete aria-autocomplete="list" aria-label="Search for a cat breed" value="Search" aria-placeholder="Search breeds"/>
+        <input type="text" name="search" id="search" autocomplete aria-autocomplete="list" aria-label="Search for a cat breed" placeholder="Search" aria-placeholder="Search breeds"/>
+        <datalist id="search">
+          <option value="Ice cream">Ice cream</option>
+          <option value="Hello">Hello</option>
+        </datalist>
         <img src={SearchIcon} className="search" alt='Search icon'/>
       </div>
     </div>
