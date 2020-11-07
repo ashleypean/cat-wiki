668515035287d48d7002170467b30bdb5f2ec470
Ashley Pean <pean.ashley@gmail.com>
2020-11-07 08:13:58 -0500

    Adjusted footer, artice, and photoGrid for medium sized screens (tablet)

--- src/components/HomePage/Header/Header.jsx
+++ src/components/HomePage/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SearchIcon from '../../../img/search-icon.png'
+import HeaderLogo from '/Users/ashley/Library/Mobile Documents/com~apple~CloudDocs/Coding/Projects/cat-wiki/src/img/logo-white.svg'
 import './Header.css'
 
 export default function Header() {
@@ -7,6 +8,7 @@ export default function Header() {
   return (
     <div className="header">
       <h4 className="title">CatWiki</h4>
+      <img src={HeaderLogo} alt=""/>
       <p className="subtitle">Get to know more about your cat breed</p>
       <div className="search">
         <input type="text" name="search" id="search" autocomplete aria-autocomplete="list" aria-label="Search for a cat breed" placeholder="Search" aria-placeholder="Search breeds"/>
