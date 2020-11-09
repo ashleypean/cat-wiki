9a6f9272e9d771fdf2f34c73ae95a55eb1bf912f
Ashley Pean <pean.ashley@gmail.com>
2020-11-09 10:19:51 -0500

    Basic layout for search page completed

--- src/components/BreedSearchResults/BreedSummary/SummaryContent/Stats.jsx
+++ src/components/BreedSearchResults/BreedSummary/SummaryContent/Stats.jsx
@@ -4,81 +4,106 @@ export default function Stats() {
   return (
     <div className="stats">
       <p>
-        <span>Temperament:</span>
-        Temperament: Alert, Agile, Energetic, Demanding, Intelligent
+        <span>Temperament: </span>
+        Alert, Agile, Energetic, Demanding, Intelligent
       </p>
       <p>
-        <span>Origin:</span>
+        <span>Origin: </span>
         United States
       </p>
       <p>
-        <span>Life Span:</span>
+        <span>Life Span: </span>
         12 - 15 years
       </p>
-      <p>
-        <span>Adaptability</span>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-      </p>
-      <p>
-        <span>Affection level:</span>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-      </p>
-      <p>
-        <span>Child Friendly:</span>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-      </p>
-      <p>
-        <span>Grooming:</span>
-        <div className="bron"></div>
-        <div className="bron"></div>
-        <div className="bron"></div>
-        <div className="bron"></div>
-        <div className="bron"></div>
-      </p>
-      <p>
-        <span>Intelligence:</span>
-        <div className="bronw"></div>
-        <div className="bronw"></div>
-        <div className="bronw"></div>
-        <div className="bronw"></div>
-        <div className="bronw"></div>
-      </p>
-      <p>
-        <span>Health issues:</span>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-      </p>
-      <p>
-        <span>Social needs:</span>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-      </p>
-      <p>
-        <span>Stranger friendly:</span>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-        <div className="brown"></div>
-      </p>
+
+      <div className="rating">
+        <span className="stat">Adaptability: </span>
+        <div className="rating-container">
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+        </div>
+      </div>
+
+      <div className="rating">
+        <span className="stat">Affection level: </span>
+        <div className="rating-container">
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+        </div>
+      </div>
+
+      <div className="rating">
+        <span className="stat">Child friendly: </span>
+        <div className="rating-container">
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+        </div>
+      </div>
+
+      <div className="rating">
+        <span className="stat">Grooming: </span>
+        <div className="rating-container">
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+        </div>
+      </div>
+
+      <div className="rating">
+        <span className="stat">Intelligence: </span>
+        <div className="rating-container">
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+        </div>
+      </div>
+
+      <div className="rating">
+        <span className="stat">Health issues: </span>
+        <div className="rating-container">
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+        </div>
+      </div>
+
+      <div className="rating">
+        <span className="stat">Social needs: </span>
+        <div className="rating-container">
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+        </div>
+      </div>
+
+      <div className="rating">
+        <span className="stat">Stranger friendly: </span>
+        <div className="rating-container">
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+          <div className="brown-bg"></div>
+        </div>
+      </div>
+
     </div>
   )
 }
