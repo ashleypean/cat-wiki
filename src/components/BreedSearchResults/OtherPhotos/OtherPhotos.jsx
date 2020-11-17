42496a3c555532d280327d8713351bd7a2175cd1
Ashley Pean <pean.ashley@gmail.com>
2020-11-17 15:23:21 -0500

    Added call to server to update extra photos

--- src/components/BreedSearchResults/OtherPhotos/OtherPhotos.jsx
+++ src/components/BreedSearchResults/OtherPhotos/OtherPhotos.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams} from 'react-router-dom'
 import Placeholder from '/Users/ashley/Library/Mobile Documents/com~apple~CloudDocs/Coding/Projects/cat-wiki/src/img/placeholder.jpg'
 import './OtherPhotos.css'
 
@@ -6,12 +7,23 @@ const photo = (
   <img src={Placeholder} alt="Placeholder"/>
 )
 
-export default function OtherPhotos() {
+export default function OtherPhotos(props) {
+  const { breedName } = useParams()
+  const [photos, setPhotos] = useState([Placeholder, Placeholder, Placeholder, Placeholder, Placeholder, Placeholder, Placeholder, Placeholder])
+
+  //Fetch photos from server
+  useEffect(() => {
+    fetch(`http://localhost:3000/breeds/search/${breedName}`)
+    .then(res => res.json())
+    .then(data => setPhotos(data.photos.slice(1)))
+    console.log(photos)
+  }, [])
+
   return (
     <div className="other-photos">
       <h3>Other photos</h3>
       <div className="photos">
-      {[photo,photo,photo,photo,photo,photo,photo,photo ]}
+      {photos.map((photo, index) => <img src={photo} alt={breedName} key={index} />)}
       </div>
       
     </div>
