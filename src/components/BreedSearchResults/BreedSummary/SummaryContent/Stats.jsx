bd54092c3d865a33fa2a75851c0a404b3836d93d
Ashley Pean <pean.ashley@gmail.com>
2020-11-17 15:05:42 -0500

    Imoprted props into comoponent to dynamically update breed info and stats

--- src/components/BreedSearchResults/BreedSummary/SummaryContent/Stats.jsx
+++ src/components/BreedSearchResults/BreedSummary/SummaryContent/Stats.jsx
@@ -1,106 +1,90 @@
 import React from 'react'
 
-export default function Stats() {
+export default function Stats(props) {
+  const { stats } = props
+
+  //Function will updates className to determine if the div has a brown or white fill 
+  const colorRatings = num => {
+    const ratingsArr = []
+    for(let i = 0; i < 5; i++) {
+      if(num > 0) {
+        ratingsArr.push(<div className="brown-bg"/>)
+      }else {
+        ratingsArr.push(<div className="no-bg"/>)
+      }
+      --num
+    }
+    return ratingsArr
+  }
+
   return (
     <div className="stats">
       <p>
         <span>Temperament: </span>
-        Alert, Agile, Energetic, Demanding, Intelligent
+        {stats.temperament}
       </p>
       <p>
         <span>Origin: </span>
-        United States
+        {stats.origin}
       </p>
       <p>
         <span>Life Span: </span>
-        12 - 15 years
+        {stats.life_span + ' years'} 
       </p>
 
       <div className="rating">
         <span className="stat">Adaptability: </span>
         <div className="rating-container">
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
+          {colorRatings(stats.adaptability).map(x => x)}
         </div>
       </div>
 
       <div className="rating">
         <span className="stat">Affection level: </span>
         <div className="rating-container">
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
+          {colorRatings(stats.affection_level).map(x => x)}
         </div>
       </div>
 
       <div className="rating">
         <span className="stat">Child friendly: </span>
         <div className="rating-container">
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
+         {colorRatings(stats.child_friendly).map(x => x)}
         </div>
       </div>
 
       <div className="rating">
         <span className="stat">Grooming: </span>
         <div className="rating-container">
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
+         {colorRatings(stats.grooming).map(x => x)}  
         </div>
       </div>
 
       <div className="rating">
         <span className="stat">Intelligence: </span>
         <div className="rating-container">
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
+          {colorRatings(stats.intelligence).map(x => x)} 
         </div>
       </div>
 
       <div className="rating">
         <span className="stat">Health issues: </span>
         <div className="rating-container">
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
+         {colorRatings(stats.health_issues).map(x => x)} 
         </div>
       </div>
 
       <div className="rating">
         <span className="stat">Social needs: </span>
         <div className="rating-container">
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
+         {colorRatings(stats.social_needs).map(x => x)} 
         </div>
       </div>
 
       <div className="rating">
         <span className="stat">Stranger friendly: </span>
         <div className="rating-container">
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
-          <div className="brown-bg"></div>
+         {colorRatings(stats.stranger_friendly).map(x => x)} 
         </div>
       </div>
 
