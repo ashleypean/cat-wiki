906986ba2fa562e3174581bcdb411485fa3df0e3
Ashley Pean <pean.ashley@gmail.com>
2020-11-18 17:00:19 -0500

    Added useEffect hook to fetch top10 cats from server. Changed search result to a function that dynamically loads all cats with a map function

--- src/components/Top10Page/Description/Description.jsx
+++ src/components/Top10Page/Description/Description.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Placeholder from '/Users/ashley/Library/Mobile Documents/com~apple~CloudDocs/Coding/Projects/cat-wiki/src/img/placeholder.jpg'
 import './Description.css'
 
-const searchResult = (
-  <div className="search-result">
-      <img src={Placeholder} alt=""/>
-      <div className="text">
-        <h3>1. Bengal</h3>
-        <p>
-        Bengals are a lot of fun to live with, but they're definitely not the cat for everyone, or for first-time cat owners. Extremely intelligent, curious and active, they demand a lot of interaction and woe betide the owner who doesn't provide it. 
-        </p>
-      </div>
-  </div>
-
-)
-
-
 
 export default function Description() {
+  const [top10, setTop10] = useState([{
+    name: 'Bengal', 
+    url: Placeholder, 
+    description: 'This is a description'
+  }])
+
+  const searchResult =(obj, i) => {
+    return (
+      <div className="search-result" key={i}>
+        <img src={obj.url} alt=""/>
+        <div className="text">
+          <h3>{`${i + 1}. ${obj.name}`}</h3>
+          <p>
+          {obj.description}
+          </p>
+        </div>
+      </div>
+    )
+  } 
+
+  useEffect(() => {
+    fetch('http://localhost:3001/top-10')
+      .then(res => res.json())
+      .then(data => setTop10(data))
+  }, [])
+
   return (
     <div className="top-10-description">
-      
-      {[searchResult,searchResult,searchResult,searchResult,searchResult,searchResult,searchResult,searchResult,searchResult,searchResult ]}
+      {top10.map((obj, index) => {
+        return searchResult(obj, index)
+      })}
     </div>
   )
 }
+
