9f16860ad47751e59d723c1946f54afe8c1712ab
Ashley Pean <pean.ashley@gmail.com>
2020-11-18 17:48:39 -0500

    Updated passed in props using desctructuring. Added a function to capitalize the first letter of the cat's name

--- src/components/BreedSearchResults/BreedSummary/SummaryContent/Text.jsx
+++ src/components/BreedSearchResults/BreedSummary/SummaryContent/Text.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-export default function Text(props) {
+export default function Text({name, description}) {
+  
+  //Name is currently written in all lowerase
+  //Capitalize the first letter of the cat's name
+  const upperCaseName = () => {
+    const arr = name.split('')
+    arr[0] = arr[0].toUpperCase()
+    name = arr.join('')
+    return name
+  }
+
   return (
     <div className="text">
-      <h4>{props.name}</h4>
-      <p>{props.description}</p>
+      <h4>{upperCaseName()}</h4>
+      <p>{description}</p>
     </div>
   )
 }
