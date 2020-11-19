994954af93dcd3885ee628e4fa98933e76e59105
Ashley Pean <pean.ashley@gmail.com>
2020-11-19 14:23:12 -0500

    Moved the footer out of all the individual components and placed it in the main App component. Fixed styling issues where html would not be 100% of the page. Issue came from the Article component in the home page having a fixed height of 62% ()

--- src/App.js
+++ src/App.js
@@ -4,33 +4,24 @@ import './App.css';
 import HomePage from './components/HomePage/HomePage.js'
 import Top10 from './components/Top10Page/Top10.js'
 import SearchResults from './components/BreedSearchResults/SearchResults.js'
+import Placeholder from '/Users/ashley/Library/Mobile Documents/com~apple~CloudDocs/Coding/Projects/cat-wiki/src/img/placeholder.jpg'
+import Footer from './components/Footer/Footer.jsx'
 
 export default function App() {
-
 
   return (
     <div className="App">
       <Router>
-        <Switch>
-          
+        <Switch>      
           {/* HOME PAGE */}
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-
+          <Route exact path="/" render={HomePage} />
           {/* MOST SEARCHED BREEDS*/}
-          <Route exact path="/top-10">
-            <Top10/>
-          </Route>
-
+          <Route exact path="/top-10" render={Top10}/>
           {/* SEARCH SPECIFIC BREED */ }
-          <Route path="/breeds/search/:breedName">
-            <SearchResults />
-          </Route>
-
+          <Route path="/breeds/search/:breedName" render={SearchResults} />
         </Switch>
       </Router>
-      
+      <Footer />
     </div>
   );
 }
