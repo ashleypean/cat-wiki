d459fd20b9d11c0d1215ee94cabe948e4493d9ed
Ashley Pean <pean.ashley@gmail.com>
2020-11-24 10:18:44 -0500

    Moved fetch calls to server to the home page component and passed the response as props to the MostSearched and Header components. Added a loading component under a setTimout method to prevent the user from seeing blank photos while the content is being fetched and rendered

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,15 +1,32 @@
+import React, { useState, useEffect } from 'react'
 import {ReactComponent as Logo} from '../../img/logo.svg'
 import Header from './Header/Header.jsx'
 import Article from './Article/Article.jsx'
 import MostSearched from './MostSearched/MostSearched.jsx'
 import PhotoGrid from './PhotoGrid/PhotoGrid.jsx'
+import LoadingPage from '../Loading/Loading.jsx'
 
-export default function Homepage({names, top4}) {
+export default function Homepage() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [names, setNames] = useState(['', ''])
+  const [top4, setTop4] = useState([{}])
+
   const divStyle={
     height: "100%", 
     width: "100%"
   }
-  return (
+  useEffect( () => {
+    fetch('http://localhost:3001/')
+      .then(res => res.json())
+      .then(data => {
+        setNames(data.names)
+        setTop4(data.top4)
+        console.log('data: ', data.names)
+      })
+      .then(setTimeout(() => setIsLoading(false), 3000))
+  }, [])
+
+  return  isLoading? <LoadingPage />: (
     <div className="homepage" style={divStyle}>
       <Logo />
       <Header names={names}/>
@@ -17,5 +34,5 @@ export default function Homepage({names, top4}) {
       <Article />
       <PhotoGrid />
     </div>
-  );
+  )
 }
