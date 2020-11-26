e50edb2b279dddbbfc64ffa998ae6ac914d00c55
Ashley Pean <pean.ashley@gmail.com>
2020-11-26 08:10:13 -0500

    Adjusted styling and structure of the 404 page

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -14,8 +14,8 @@ export default function NotFound() {
   return (
     <div className="not-found">
       <h1>Uh Oh! <br /> It looks like this page doesn't exist. Let's try something else.</h1>
-      <img src={uhOh} alt="Cat with shocked face and text that says uh oh"/>
       <button onClick={handleClick}> Go Back Home </button>
+      <img src={uhOh} alt="Cat with shocked face and text that says uh oh"/>
     </div>
   )
 }
